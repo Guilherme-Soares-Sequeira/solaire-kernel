b83d4d218a2efda8a7158ceeb34b272c1f0ca330
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2024-06-12 22:05:49 +0100

    feat: man made sleep LIKE A BABUSSY

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,12 @@
 #include "include/kernel.h"
 
 
-void t1(void) { while (TRUE) { toggle_led(LED3); end_cycle(); } }
-void t2(void) { while (TRUE) { toggle_led(LED4); end_cycle(); } }
-void t3(void) { while (TRUE) { toggle_led(LED4); end_cycle(); } }
-
-//extern int32_t sys_clock;
+void t1(void) { while (TRUE) { toggle_led(LED1); end_cycle(); } }
+void t2(void) { while (TRUE) { toggle_led(LED2); end_cycle(); } }
+void t3(void) { while (TRUE) { toggle_led(LED3); end_cycle(); } }
 
 void task_main(void) {
     disable_interrupts();
-
-    // Serial.println("in task_main");
-    // Serial.flush();
 
     pin_md(LED1, OUTPUT);
     pin_md(LED2, OUTPUT);
@@ -36,23 +31,18 @@ void task_main(void) {
 
     int16_t idx_t1 = create(TASK1_NAME, t1, TASK_TYPE_PERIODIC, TASK1_CRIT, TASK1_PERIOD, TASK1_WCET);
     int16_t idx_t2 = create(TASK2_NAME, t2, TASK_TYPE_PERIODIC, TASK2_CRIT, TASK2_PERIOD, TASK2_WCET);
-    //int16_t idx_t3 = create(TASK3_NAME, t3, TASK_TYPE_PERIODIC, TASK3_CRIT, TASK3_PERIOD, TASK3_WCET);
-
-    // Serial.println("Activating tasks...\n");
-    // Serial.flush();
+    int16_t idx_t3 = create(TASK3_NAME, t3, TASK_TYPE_PERIODIC, TASK3_CRIT, TASK3_PERIOD, TASK3_WCET);
     
     activate(idx_t1);
     activate(idx_t2);
-    //activate(idx_t3);     
+    activate(idx_t3);     
     
-    // Serial.println("All tasks have been activated successfully!");
-    // Serial.flush();
-
     enable_interrupts();
-    /* busy waiting */
+
     while (TRUE) {
-        // toggle_led(LED4);
-        asm volatile("nop");
+        toggle_led(LED4);
+        for (uint16_t i = 0; i < 65000; i++)
+            asm volatile("nop");
     }
 
     return;
